62501a92229aa6379fc6249b9827b8128dfccc60
Mulatyangio <mulatyamichael6@gmail.com>
2024-06-04 18:29:07 +0300

    Ngio Restcountry

--- rest-countries-api-with-color-theme-switcher-master/index.js
+++ rest-countries-api-with-color-theme-switcher-master/index.js
@@ -1,4 +1,4 @@
-fetch(data.json)
+fetch('data.json')
 .then(response => response.json())
 .then(data =>{
     
@@ -9,14 +9,23 @@ fetch(data.json)
 
 
                 countries.forEach(country => {
-                    let liElement=document.createElement("li")
-                    liElement.innerHTML `
-                    <img src="${country.image}" alt="${country.name}">
+                    let liElement=document.createElement("div")
+                    liElement.innerHTML= `
+                    <img src="${country.flag}" alt="${country.name}">
                     <h3>${country.name}</h3>
                     <p>${country.population}</p>
                     <p>${country.region}</p>
                     <p>${country.capital}</p>
                     `
+                    document.querySelector('.countries').append(liElement)
 
         })
     }
+
+
+ const modeToggle=document.getElementById("mode-toggle")
+ const body=document.body
+ modeToggle.addEventListener('click',()=>{
+    body.classList.toggle('dark-mode') 
+    body.classList.toggle('light-mode')
+ });
